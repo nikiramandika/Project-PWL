58019df2949c0212eee52b29b73a2e7f0bf8f056
naviolyn <artemisathena456@gmail.com>
2024-05-15 11:45:38 +0700

    urlquery

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -5,19 +5,24 @@ namespace App\Livewire;
 use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
-use Livewire\Component;
+use Livewire\Attributes\Url;
 use Livewire\Attributes\Title;
+use Livewire\Component;
 use Livewire\WithPagination;
+
 
 #[Title('Products - PPWL')]
 class ProductsPage extends Component
 {
     use WithPagination;
-    public function render()
-    {
+
+    #[Url]
+    public $selected_categories = [];
+
+    public function render(){
         $productQuery = Product::query()->where('is_active', 1);
         return view('livewire.products-page', [
-            'products' => $productQuery->paginate(6),
+            'products' => $productQuery->paginate(9),
             'brands' => Brand::where('is_active', 1)->get(['id', 'name', 'slug']),
             'categories' => Category::where('is_active', 1)->get(['id', 'name', 'slug']),
         
--- resources/views/livewire/products-page.blade.php
+++ resources/views/livewire/products-page.blade.php
@@ -5,12 +5,13 @@
           <div class="w-full pr-2 lg:w-1/4 lg:block">
             <div class="p-4 mb-5 bg-white border border-gray-200 dark:border-gray-900 dark:bg-gray-900">
               <h2 class="text-2xl font-bold dark:text-gray-400"> Categories</h2>
+              {{json_encode($selected_categories)}}
               <div class="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
               <ul>
                 @foreach ($categories as $category)
                   <li class="mb-4" wire:key="{{ $category->id}}">
                     <label for="{{ $category->slug}}" class="flex items-center dark:text-gray-400 ">
-                      <input type="checkbox" id="{{ $category->slug}}" value="{{ $category->id}}" class="w-4 h-4 mr-2">
+                      <input type="checkbox" wire:model.live="selected_categories" id="{{ $category->slug}}" value="{{ $category->id}}" class="w-4 h-4 mr-2">
                       <span class="text-lg">{{ $category->name}}</span>
                     </label>
                   </li>
