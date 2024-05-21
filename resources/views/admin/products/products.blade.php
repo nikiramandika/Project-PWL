c19e980b81f1746f47d7c46d184dc1c50de9535d
nikiramandika <nikiramandika@icloud.com>
2024-05-21 11:26:51 +0700

    update title

--- resources/views/admin/products/products.blade.php
+++ resources/views/admin/products/products.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.user_type.auth')
 
+@section('title', 'Dashboard')
 @section('content')
 <div>
     <style>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.user_type.auth')
 
+@section('title', 'Dashboard')
 @section('content')
     <div class="row">
         <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
